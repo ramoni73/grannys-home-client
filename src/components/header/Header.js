e8c65d4f597259bb36e9528ef54b57f6797dd61f
roman <ramoni1990@gmail.com>
2020-06-04 18:43:19 +0300

    fix menu header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,12 +6,12 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <Link className="navbar-brand" to="/">Granny's Home</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
-                    aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarColor01">
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
                         <Link className="nav-link" to="/offers/">Предложения</Link>
